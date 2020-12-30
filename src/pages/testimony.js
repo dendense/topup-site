976981c0c1993f03abff81b4412a2adef283e8cc
DenDense <denyfarras@gmail.com>
2020-12-30 10:55:24 +0700

    adding testimony page

---
+++ src/pages/testimony.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import SEO from "../components/Seo"
+import Layout from "../components/Layout"
+
+export default function Testimony() {
+  return (
+    <Layout>
+      <SEO title="Testimony" />
+    </Layout>
+  )
+}
