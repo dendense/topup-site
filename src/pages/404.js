691d0440c5aaa2c88712110044ece76070b2980d
DenDense <denyfarras@gmail.com>
2020-12-31 08:54:10 +0700

    Fix 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,14 +6,12 @@ import SEO from "../components/Seo"
 const notfound = () => (
   <div className="d-flex align-items-center mt-3">
     <SEO title="Page Not Found" />
-    <div className="container d-flex flex-column">
-      <div className="d-flex justify-content-center">
-        <img src="https://i.ibb.co/yVr4pnk/5.png" alt="404 Not Found" />
-      </div>
-      <div className="d-flex justify-content-center">
-        <h2>This Page is Lost in Space</h2>
-      </div>
-      <div className="d-flex justify-content-center">
+    <div
+      className="container d-flex justify-content-center flex-column text-center"
+      style={{ marginTop: "6em" }}
+    >
+      <h2>This Page is Lost in Space</h2>
+      <div>
         <Link to="/" className="btn btn-primary">
           Back to Home
         </Link>
