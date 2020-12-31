128cb8d2cc56edc76889bb4e1be1c8aae0bf0ed0
DenDense <denyfarras@gmail.com>
2020-12-31 08:59:21 +0700

    Change 404 Page to Functional Component

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,21 +3,21 @@ import { Link } from "gatsby"
 
 import SEO from "../components/Seo"
 
-const notfound = () => (
-  <div className="d-flex align-items-center mt-3">
-    <SEO title="Page Not Found" />
-    <div
-      className="container d-flex justify-content-center flex-column text-center"
-      style={{ marginTop: "6em" }}
-    >
-      <h2>This Page is Lost in Space</h2>
-      <div>
-        <Link to="/" className="btn btn-primary">
-          Back to Home
-        </Link>
+export default function NotFound() {
+  return (
+    <div className="d-flex align-items-center mt-3">
+      <SEO title="Page Not Found" />
+      <div
+        className="container d-flex justify-content-center flex-column text-center"
+        style={{ marginTop: "6em" }}
+      >
+        <h2>This Page is Lost in Space</h2>
+        <div>
+          <Link to="/" className="btn btn-primary">
+            Back to Home
+          </Link>
+        </div>
       </div>
     </div>
-  </div>
-)
-
-export default notfound
+  )
+}
